a8676a7e799dfc9f3de40d536ff7e31086c8ae1b
Vince <vince.qmarketz@gmail.com>
2021-04-17 19:02:03 +0800

    fixed time gmt+8 bug

--- src/components/Modals/forms/data/appointmentData.js
+++ src/components/Modals/forms/data/appointmentData.js
@@ -2,10 +2,10 @@
 
 export const FIELD_TIME = [
   { id: "01:00:00", label: "9 AM" },
-  { id: "2:00:00", label: "10 AM" },
-  { id: "3:00:00", label: "11 AM" },
-  { id: "5:00:00", label: "1 PM" },
-  { id: "6:00:00", label: "2 PM" },
-  { id: "7:00:00", label: "3 PM" },
-  { id: "8:00:00", label: "4 PM" },
+  { id: "02:00:00", label: "10 AM" },
+  { id: "03:00:00", label: "11 AM" },
+  { id: "05:00:00", label: "1 PM" },
+  { id: "06:00:00", label: "2 PM" },
+  { id: "07:00:00", label: "3 PM" },
+  { id: "08:00:00", label: "4 PM" },
 ];
