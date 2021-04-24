c7fb64b7d1e746d48cc0615ac4b307abc8b0e6a8
Vince <vince.qmarketz@gmail.com>
2021-04-25 06:29:34 +0800

    removed accordion(FAQs) shadow when in Closed state

--- config/index.js
+++ config/index.js
@@ -14,6 +14,19 @@ const theme = {
         whiteSpace: "pre-wrap",
       },
     },
+    MuiAccordion: {
+      root: {
+        boxShadow: "none !important",
+        "&::before": {
+          top: 0,
+        },
+      },
+    },
+    MuiCollapse: {
+      container: {
+        boxShadow: `0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%);`,
+      },
+    },
   },
   palette: {
     primary: {
